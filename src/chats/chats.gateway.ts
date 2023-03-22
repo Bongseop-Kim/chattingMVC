25d123c146e4314bdc9a102a8187723f953302c4
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-22 16:20:28 +0900

    Gateway 생명 주기 connect, disconnect, afterInit 사용하기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,13 +1,36 @@
+import { Logger } from '@nestjs/common';
+import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Socket } from 'dgram';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected: ${socket.id} ${socket.nsp.name}`);
+  }
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected: ${socket.id} ${socket.nsp.name}`);
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
