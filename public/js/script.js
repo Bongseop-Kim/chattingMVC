e8a64739e905e9613667cca0db993da889c57e61
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-22 15:40:20 +0900

    Socket 세팅 (프론트, 백 둘 다 )

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,16 @@
-alert('hello world');
+const socket = io('/');
+const getElementById = (id) => document.getElementById(id) | null;
+
+const helloStrangerElement = getElementById('hello_stranger');
+const cgattubgBoxElement = getElementById('chatting_box');
+const forElement = getElementById('chat_form');
+
+function helloUser() {
+  const username = prompt('What is youre name?');
+}
+
+function init() {
+  helloUser();
+}
+
+init();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { AppController } from './app.controller';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AppController } from './app.controller';
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
+    ChatsModule,
   ],
   controllers: [AppController],
 })
