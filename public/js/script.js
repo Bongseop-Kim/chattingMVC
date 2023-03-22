1617a55960cd7438acfba5b13e0d360703e66c5d
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-22 16:57:19 +0900

    이벤트 설계와 기본 서비스 로직

--- public/js/script.js
+++ public/js/script.js
@@ -2,16 +2,34 @@ const socket = io('/chattings');
 const getElementById = (id) => document.getElementById(id) || null;
 
 const helloStrangerElement = getElementById('hello_stranger');
-const cgattubgBoxElement = getElementById('chatting_box');
-const forElement = getElementById('chat_form');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
 
 //global 소켓 핸들러
-socket.on('user_connected', (username) =>
-  console.log(`${username} connected! `),
-);
+socket.on('user_connected', (username) => drawChat(`${username} connected! `));
+socket.on('submit_chat', (data) => {
+  const { chat, username } = data;
+  drawChat(`${username} : ${chat}`);
+});
 
-const drawHelloStranger = (username) => {
-  helloStrangerElement.innerText = `Hello ${username} Strnager :)`;
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const inputValue = e.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    drawChat(`me: ${inputValue}`);
+    e.target.elements[0].value = '';
+  }
+};
+
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} Strnager :)`);
+
+const drawChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `<div>${message}</div>`;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
 };
 
 function helloUser() {
@@ -23,6 +41,7 @@ function helloUser() {
 
 function init() {
   helloUser();
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
