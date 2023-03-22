1da9de0a55c686b8abb5d219416c5e13e7d14610
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-22 16:40:06 +0900

    접속한 모든 클라이언트에 알리는 broadcasting

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,24 @@
 const socket = io('/chattings');
-const getElementById = (id) => document.getElementById(id) | null;
+const getElementById = (id) => document.getElementById(id) || null;
 
 const helloStrangerElement = getElementById('hello_stranger');
 const cgattubgBoxElement = getElementById('chatting_box');
 const forElement = getElementById('chat_form');
 
+//global 소켓 핸들러
+socket.on('user_connected', (username) =>
+  console.log(`${username} connected! `),
+);
+
+const drawHelloStranger = (username) => {
+  helloStrangerElement.innerText = `Hello ${username} Strnager :)`;
+};
+
 function helloUser() {
   const username = prompt('What is youre name?');
-  socket.emit('new_user', username);
-  socket.on('hello_user', (data) => console.log(data));
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
+  });
 }
 
 function init() {
